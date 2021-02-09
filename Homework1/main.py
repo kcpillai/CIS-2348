fccc3f30be4e0914e0a28e93ece1eb0cbd6d090d
kcpillai <77808972+kcpillai@users.noreply.github.com>
2021-02-08 20:23:10 -0600

    Add files via upload

---
+++ Homework1/main.py
@@ -0,0 +1,14 @@
+#Kiran Pillai
+#1780014
+
+user_num = int(input("Enter integer:\n"))
+
+
+print("You entered:", user_num)
+print("{user_num} squared is", user_num * user_num)
+print("And {user_num} cubed is {user_num * user_num * user_num} !!")
+
+other_num = int(input("Enter another integer:\n"))
+
+print("{user_num} + {other_num} is", user_num + other_num)
+print("{user_num} * {other_num} is", user_num * other_num)
